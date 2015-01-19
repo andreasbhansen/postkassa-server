592df0a1e212b472e222ea1150c4a0c16e2d3d8b
andreasbhansen <andreasb.nor@gmail.com>
2015-01-19 09:52:57 +0100

    Additional changes

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ var express = require('express');
 var pushWoosh = require('pushwoosh');
 var pushClient = new pushWoosh("0456C-F5F07", "SxUGjDOZqIGFAFjpskYwDgeY5Ulm4J3R9IoZWmsBh2l1G1Q2lhxxBk5MbSbjonzEgRc0skBXpgEtbdbKCWzo");
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mailbox');
 
 var app = express();
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json());
 
 // Schemas
-var mailboxSchema = new mongoose.Schema({mailbox_id: String, mailbox_name: String, devices: [String]});
+var mailboxSchema = new mongoose.Schema(
+    {
+        mailbox_id: String,
+        mailbox_name: String,
+        devices: [String]
+    }
+);
 
 // Models
 var Mailbox = mongoose.model('Mailbox', mailboxSchema, 'mailboxes');
@@ -32,6 +39,8 @@ app.post('/send-push', function (req, res)
             "devices": mailbox.devices
         };
 
+        console.log(config);
+
         pushClient.sendMessage(config).then(function (data)
         {
             res.setHeader('Content-Type', 'application/json');
@@ -40,8 +49,11 @@ app.post('/send-push', function (req, res)
     });
 });
 
-var devices = [];
-
+/*
+    ::HTTPcode + meaning in this context::
+    200: Found, as in "already existing"
+    201: Created
+ */
 
 app.get('/', function (req, res)
 {
@@ -58,22 +70,29 @@ app.post('/register-device', function (req, res)
     console.log(mailbox_id);
     console.log(token);
 
-    Mailbox.update(
-        {mailbox_id: mailbox_id},
-        {$push: {"devices": token}},
-        {safe: true, upsert: true},
-        function (err, model)
-        {
-            if (err)
-            {
-                console.log(err);
-            }
+    Mailbox.findOne({mailbox_id: mailbox_id}, function (err, mailbox) {
+        if(_.contains(mailbox.devices, token)) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({status_code: 200, message: 'You already subscribe to notifications for this mailbox.'}, null, 3));
+        } else {
 
-            console.log(model);
-        });
-
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({status_code: 200}, null, 3));
+            Mailbox.update(
+                {mailbox_id: mailbox_id},
+                {$push: {"devices": token}},
+                {safe: true, upsert: true},
+                function (err, model)
+                {
+                    if (err)
+                    {
+                        console.log(err);
+                    } else
+                    {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.end(JSON.stringify({status_code: 201, message: 'You successfully subscribed to notifications for this mailbox.'}, null, 3));
+                    }
+                });
+        }
+    });
 });
 
 app.get('/fetch-mailboxes', function (req, res)
@@ -161,14 +180,8 @@ app.post('/register-mailbox', function (req, res)
     })
 });
 
-app.get('/test', function (req, res)
-{
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({devices: devices}, null, 3));
-});
 
-
-app.listen(8080, function ()
+app.listen(7654, function ()
 {
     console.log("Check it out!");
 });
